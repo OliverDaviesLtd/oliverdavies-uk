418b056cd2c642f159da461d17e75d3f3545b529
Oliver Davies <oliver@oliverdavies.uk>
2020-04-24 08:59:32 +0100

    Add run-tests.sh

---
+++ bin/run-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+symfony php vendor/bin/phpcs -n \
+  --standard="Drupal,DrupalPractice" \
+  --extensions="php,module,inc,install,test,profile,theme" \
+  --ignore=*/tests/* \
+  --exclude="Drupal.Commenting.ClassComment,Drupal.Commenting.FunctionComment" \
+  web/modules/custom
+
+# symfony php vendor/bin/phpcs -ns \
+#   --standard="Drupal,DrupalPractice" \
+#   --extensions="php,module,inc,install,test,profile,theme" \
+#   --exclude="Drupal.Commenting.ClassComment,Drupal.Commenting.DocComment,Drupal.Commenting.FunctionComment,Drupal.NamingConventions.ValidFunctionName" \
+#   web/modules/custom/**/tests
+
+vendor/bin/phpstan analyze
