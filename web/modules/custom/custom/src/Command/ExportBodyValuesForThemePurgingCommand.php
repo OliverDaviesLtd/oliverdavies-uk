f3f5557cb6f345432a1c7d43679b6c987c426a2d
Oliver Davies <oliver@oliverdavies.uk>
2020-05-20 01:39:21 +0100

    wip

--- web/modules/custom/custom/src/Command/ExportBodyValuesForThemePurgingCommand.php
+++ web/modules/custom/custom/src/Command/ExportBodyValuesForThemePurgingCommand.php
@@ -4,23 +4,46 @@ declare(strict_types=1);
 
 namespace Drupal\custom\Command;
 
+use Drupal\Core\Database\Connection;
 use Illuminate\Support\Collection;
 
 final class ExportBodyValuesForThemePurgingCommand {
 
+  private static array $tableNames = [
+    'block_content__body',
+    'node__body',
+  ];
+
+  private string $filename = 'body-field-values.txt';
+
+  private Connection $database;
+
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
   /**
-   * Drush command for updating legacy tag names.
+   * Drush command to export body field values into a file.
    *
    * @command opdavies:export-body-values-for-theme-purging
    */
-  public function exportBodyValues() {
-    $blockValues = db_query('SELECT body_value FROM {block_content__body}')->fetchCol();
-    $nodeValues = db_query('SELECT body_value FROM {node__body}')->fetchCol();
-
-    $values = (new Collection([...$blockValues, ...$nodeValues]))
+  public function handle() {
+    $values = Collection::make(self::$tableNames)
+      ->flatMap(fn(string $tableName) => $this->getValuesFromTable($tableName))
       ->implode(PHP_EOL);
 
-    file_put_contents(drupal_get_path('theme', 'opdavies') . '/body-field-values.txt', $values);
+    file_put_contents($this->getFilePath(), $values);
+  }
+
+  private function getFilePath(): string {
+    return drupal_get_path('theme', 'opdavies') . DIRECTORY_SEPARATOR . $this->filename;
+  }
+
+  private function getValuesFromTable(string $tableName): array {
+    return $this->database->select($tableName)
+      ->fields($tableName, ['body_value'])
+      ->execute()
+      ->fetchCol();
   }
 
 }
