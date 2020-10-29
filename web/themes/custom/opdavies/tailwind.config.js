530074f176a780a26cd7c3591fd3035feb610e63
Oliver Davies <oliver@oliverdavies.uk>
2020-10-29 00:46:52 +0000

    Override gray colours
    Override the default Tailwind `gray` colours, and update anywhere that
    the originals were used.
    
    References #27

--- web/themes/custom/opdavies/tailwind.config.js
+++ web/themes/custom/opdavies/tailwind.config.js
@@ -59,7 +59,17 @@ module.exports = {
     }),
     extend: {
       colors: {
-        inherit: 'inherit'
+        inherit: 'inherit',
+
+        gray: {
+          50: "#f9f9f9",
+          100: "#f5f5f5",
+          200: "#aaa",
+          700: "#36393e",
+          750: "#2e3136",
+          800: "#1e2125",
+          900: "#18171b",
+        }
       },
       fontFamily: {
         mono: [
