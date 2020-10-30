4008f511f8093a7d6b15cf0ecc820195f50e7436
Oliver Davies <oliver@oliverdavies.uk>
2020-10-30 00:21:59 +0000

    Move the config sync directory

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -758,7 +758,7 @@ $settings['entity_update_backup'] = TRUE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 
-$settings["config_sync_directory"] = '../config/default';
+$settings["config_sync_directory"] = '../config';
 
 if (isset($_SERVER['SYMFONY_DEFAULT_ROUTE_URL']) && file_exists(__DIR__ . '/environments/settings.symfony.php')) {
   require_once __DIR__ . '/environments/settings.symfony.php';
