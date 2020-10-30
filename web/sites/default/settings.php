e76251c3760d39dedcc6bcb4d60e8be11552c638
Oliver Davies <oliver@oliverdavies.uk>
2020-10-30 01:00:23 +0000

    Remove require for old Symfony settings file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -760,10 +760,6 @@ $settings['entity_update_backup'] = TRUE;
 
 $settings["config_sync_directory"] = '../config';
 
-if (isset($_SERVER['SYMFONY_DEFAULT_ROUTE_URL']) && file_exists(__DIR__ . '/environments/settings.symfony.php')) {
-  require_once __DIR__ . '/environments/settings.symfony.php';
-}
-
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
