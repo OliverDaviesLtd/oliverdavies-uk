265b6f17caeccdd13bac40a47f36ff04008abed6
Oliver Davies <oliver@oliverdavies.uk>
2020-12-16 23:52:30 +0000

    Improve syntax highlighting

--- web/themes/custom/opdavies/assets/js/app.js
+++ web/themes/custom/opdavies/assets/js/app.js
@@ -1,7 +1,20 @@
 import 'alpinejs'
 import 'focus-visible'
 import 'styles/tailwind.pcss'
-import hljs from 'highlightjs'
+import hljs from 'highlight.js/lib/core'
+import 'highlight.js/styles/hybrid.css'
+
+import bash from 'highlight.js/lib/languages/bash'
+import ini from 'highlight.js/lib/languages/ini'
+import javascript from 'highlight.js/lib/languages/javascript'
+import php from 'highlight.js/lib/languages/php'
+import yaml from 'highlight.js/lib/languages/yaml'
+
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('ini', ini);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('yaml', yaml);
 
 document.addEventListener('DOMContentLoaded', event => {
   document.querySelectorAll('pre code').forEach(block => {
