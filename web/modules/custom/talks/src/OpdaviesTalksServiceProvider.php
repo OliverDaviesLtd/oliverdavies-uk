53ab3264150f2ed5d93c7d3abca8061f85df1472
Oliver Davies <oliver@oliverdavies.uk>
2021-01-13 23:10:38 +0000

    Auto-configure services and repositories
    Enable the auto-configuration of service and repository classes,
    including support for classes in subdirectories by using
    `getRelativePathname()` rather than `getFilename()` and making some
    additional changes to the result.
    
    References #347

---
+++ web/modules/custom/talks/src/OpdaviesTalksServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\opdavies_talks;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Finder\Finder;
+
+final class OpdaviesTalksServiceProvider implements ServiceProviderInterface {
+
+  public function register(ContainerBuilder $container): void {
+    foreach (['Repository', 'Service'] as $directory) {
+      $files = Finder::create()
+        ->in(__DIR__ . '/' . $directory)
+        ->files()
+        ->name('*.php');
+
+      foreach ($files as $file) {
+        $class = 'Drupal\opdavies_talks\\' . $directory . '\\' .
+          str_replace('/', '\\', substr($file->getRelativePathname(), 0, -4));
+
+        if ($container->hasDefinition($class)) {
+          continue;
+        }
+
+        $definition = new Definition($class);
+        $definition->setAutowired(TRUE);
+        $container->setDefinition($class, $definition);
+      }
+    }
+  }
+
+}
