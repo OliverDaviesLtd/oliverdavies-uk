9990fdeab635af0c3ee7a16603d7b8c13016f1df
Oliver Davies <oliver@oliverdavies.uk>
2021-01-20 01:48:27 +0000

    Remove DDEV

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -764,7 +764,3 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 
-// Automatically generated include for settings managed by ddev.
-if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
-  include $app_root . '/' . $site_path . '/settings.ddev.php';
-}
