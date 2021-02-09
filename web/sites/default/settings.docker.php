29d88893fa4b9d37be2cdbd397f04946c07c73d0
Oliver Davies <oliver@oliverdavies.uk>
2021-02-09 12:00:00 +0000

    Add Docker and Docker Compose

---
+++ web/sites/default/settings.docker.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = [
+  'driver' => 'mysql',
+  'host' => 'mysql',
+  'database' => getenv('MYSQL_DATABASE'),
+  'username' => getenv('MYSQL_USER'),
+  'password' => getenv('MYSQL_PASSWORD'),
+  'port' => '3306',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+];
+
+$settings['trusted_host_patterns'] = [
+  '^oliverdavies.docker.localhost$',
+];
