f36a24b2b0da71b4e4684c0adbf25dc1a1098908
Oliver Davies <oliver@oliverdavies.uk>
2021-11-26 12:37:03 +0000

    chore: update Tailwind configuration
    Refs: #441

--- web/themes/custom/opdavies/tailwind.config.js
+++ web/themes/custom/opdavies/tailwind.config.js
@@ -1,85 +1,38 @@
-const aspectRatio = require('@tailwindcss/aspect-ratio')
-const colors = require('./tailwind-colours')
+const colors = require('./tailwindcss/colours')
 const defaultTheme = require('tailwindcss/defaultTheme')
-const focusVisible = require('./tailwind-plugin-focus-visible')
-const forms = require('@tailwindcss/forms')
-const typography = require('@tailwindcss/typography')
 const { fontFamily } = defaultTheme
 
 module.exports = {
-  darkMode: false,
+  mode: 'jit',
+  darkMode: 'media',
   important: true,
   purge: {
     content: [
-      '../../../../config/**/*.yml',
-      'opdavies.theme',
-      'tailwind-safelist-classes.txt',
-      'templates/**/*.twig',
+      'templates/**/*.html.twig',
+      'tools/tailwindcss/safelist-classes.txt'
     ],
   },
   theme: {
+    outline: {
+      black: '1px solid black',
+      white: '1px solid white'
+    },
     extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.blue.700')
-            },
-            code: {
-              backgroundColor: theme('colors.gray.150'),
-              fontWeight: theme('fontWeight.normal'),
-              paddingBottom: theme('spacing.px'),
-              paddingLeft: theme('spacing.1'),
-              paddingRight: theme('spacing.1'),
-              paddingTop: theme('spacing.px')
-            },
-            h2: {
-              marginBottom: theme('spacing.2'),
-              marginTop: theme('spacing.8')
-            },
-            pre: {
-              backgroundColor: theme('colors.gray.150'),
-              borderRadius: '0',
-              color: theme('colors.gray.800'),
-              padding: theme('spacing.6')
-            },
-            'code::before': false,
-            'code::after': false,
-            'pre code::before': false,
-            'pre code::after': false
-          }
-        },
-        dark: {
-          css: {
-            color: theme('colors.gray.200'),
-
-            code: {
-              backgroundColor: theme('colors.gray.750'),
-              color: theme('colors.gray.200')
-            },
-
-            'blockquote, h2, h3': {
-              color: theme('colors.white')
-            },
-
-            strong: {
-              color: theme('colors.gray.200')
-            }
-          }
-        }
-      }),
       colors,
       fontFamily: {
         sans: [
-          'Inter',
+          'Roboto Condensed',
           'Arial',
           'Helvetica Neue',
           'Helvetica',
           'sans-serif',
         ],
         mono: [
-          'Operator Mono',
-          'Roboto Mono',
+          'ui-monospace',
+          'SFMono-Regular',
+          'SF Mono',
+          'Consolas',
+          'Liberation Mono',
           ...fontFamily.mono
         ]
       },
@@ -98,9 +51,9 @@ module.exports = {
     container: false
   },
   plugins: [
-    aspectRatio,
-    focusVisible,
-    forms,
-    typography
+    require('./tailwindcss/plugins/focus-visible'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
   ]
 }
---
+++ web/themes/custom/opdavies/tailwindcss/colours.js
@@ -0,0 +1,13 @@
+let { gray } = require('tailwindcss/colors')
+
+module.exports = {
+  black: '#000',
+  blue: {
+    primary: '#24608A',
+  },
+  current: 'currentColor',
+  gray,
+  inherit: 'inherit',
+  transparent: 'transparent',
+  white: '#fff'
+}
